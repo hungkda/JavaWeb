b4926360e482f60769e4a3fec164e0158cfa0984
unknown <mhung4011@gmail.com>
2024-01-25 09:06:47 +0700

    Day5

---
+++ Lab04_Ex/src/main/java/hung/conn/ConnectionUtils.java
@@ -0,0 +1,35 @@
+package hung.conn;
+
+import java.sql.*;
+
+public class ConnectionUtils {
+	public static Connection getMSSQLConnection() throws SQLException, ClassNotFoundException {
+        String hostName = "localhost";
+        String sqlInstanceName = "ADMIN";
+        String dbName = "Lab02Servlet";
+        String userName = "sa";
+        String password = "sa123";
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String connectionURL = "jdbc:sqlserver://" + hostName + ":1433;instance=" + sqlInstanceName + ";databaseName="+dbName + ";encrypt=true;trustServerCertificate=true";
+        // Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+         //DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());  
+        Connection conn = DriverManager.getConnection(connectionURL, userName, password);
+        return conn;
+    }
+	
+	public static void closeQuietly(Connection conn) {
+		try {
+			conn.close();
+		}catch (Exception e) {
+			
+		}
+	}
+	
+	public static void rollbackQuietly(Connection conn) {
+		try {
+			conn.rollback();
+		}catch (Exception e) {
+			
+		}
+	}
+}
